3e3ef6f3d3ca25a3b6b15f49df09eb7d904450fb
rishab-talla <connect.rishab.talla@gmail.com>
2023-10-05 01:30:57 +0530

    banner-images-updated

--- client/src/components/Carousel/SliderData.js
+++ client/src/components/Carousel/SliderData.js
@@ -1,27 +1,27 @@
 export const SliderData = [
    {
       image:
-         'https://rukminim1.flixcart.com/flap/1688/280/image/4d6f68fd7c030233.jpg?q=50',
+         'https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/5df65ad101e18dbf.jpg?q=20',
       alt: 'Banner Image',
    },
    {
       image:
-         'https://rukminim1.flixcart.com/flap/1688/280/image/b2ebeaa38d2056bc.jpg?q=50',
+         'https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/10afacd24f63e5a4.jpg?q=20',
       alt: 'Banner Image',
    },
    {
       image:
-         'https://rukminim1.flixcart.com/flap/1688/280/image/c9b64a2487babb69.jpg?q=50',
+         'https://m.media-amazon.com/images/I/71U-Q+N7PXL._SX3000_.jpg',
       alt: 'Banner Image',
    },
    {
       image:
-         'https://rukminim1.flixcart.com/flap/1688/280/image/09091f15283a4239.jpg?q=50',
+         'https://p1-ofp.static.pub/ShareResource/legion/legion-2023-imgs/lenovo_legion_9i_slidea.jpg',
       alt: 'Banner Image',
    },
    {
       image:
-         'https://rukminim1.flixcart.com/flap/1688/280/image/2588832781aafce3.jpg?q=50',
+         'https://www.boat-lifestyle.com/cdn/shop/files/Deadpool_Web_1_1440x.jpg?v=1695626645',
       alt: 'Banner Image',
    },
 ];
